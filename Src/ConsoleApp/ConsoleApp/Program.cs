2fe08d63c44f74dbdf5bf2d45f5a131bd261eb6e
Martin Käck <martinkack@hotmail.com>
2019-10-25 16:55:26 +0200

    Changed to Hello World6

--- Src/ConsoleApp/ConsoleApp/Program.cs
+++ Src/ConsoleApp/ConsoleApp/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World3!");
+            Console.WriteLine("Hello World6!");
         }
     }
 }
